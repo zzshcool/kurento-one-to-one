ab0f828132d81c95276b9c9e646b2ff275af2b75
Jasoncc.Chen <Jasoncc.Chen@csnt.com.tw>
2020-01-20 11:53:37 +0800

    initialize

---
+++ src/main/java/com/example/one2one/UserRegistry.java
@@ -0,0 +1,55 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.one2one;
+
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class UserRegistry {
+
+    private ConcurrentHashMap<String, UserSession> usersByName = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, UserSession> usersBySessionId = new ConcurrentHashMap<>();
+
+    public void register(UserSession user) {
+        usersByName.put(user.getName(), user);
+        usersBySessionId.put(user.getSession().getId(), user);
+    }
+
+    public UserSession getByName(String name) {
+        return usersByName.get(name);
+    }
+
+    public UserSession getBySession(WebSocketSession session) {
+        return usersBySessionId.get(session.getId());
+    }
+
+    public boolean exists(String name) {
+        return usersByName.keySet().contains(name);
+    }
+
+    public UserSession removeBySession(WebSocketSession session) {
+        final UserSession user = getBySession(session);
+        if (user != null) {
+            usersByName.remove(user.getName());
+            usersBySessionId.remove(session.getId());
+        }
+        return user;
+    }
+
+}
